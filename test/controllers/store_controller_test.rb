70df8bf284ea7815d6afab9cc554bdd5c7395580
Nick <nramser21@gmail.com>
2016-12-09 13:02:19 -0500

    Add store indez action, view, styles, tests

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
-
+# Store controller tests
 class StoreControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get store_index_url
     assert_response :success
+    assert_select '#columns #side a', minumum: 4
+    assert_select '#main .entry', 3
+    assert_select 'h3', 'MIB Neuralyzer'
+    assert_select '.price', %r{\$[,\d]+\.\d\d}
   end
 
 end
